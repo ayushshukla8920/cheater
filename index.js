7f13969f41d3a71108468df036153cee64048ccf
Ayush Shukla <ayushshukla8920@gmail.com>
2025-04-29 16:01:03 +0530

    done

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname+'/public'));
+// app.use(express.static(__dirname+'/public'));
+
+app.get('/:filename',(req,res)=>{
+    const file = req.params.filename;
+    res.sendFile(__dirname+'/public/'+file);
+})
 
 app.listen(3000,()=>{
     console.log(`Cheating server running`);
